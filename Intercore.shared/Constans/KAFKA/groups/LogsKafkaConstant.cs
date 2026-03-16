12440873a95352f1589ad99f9350e9f3372e2a7d
MatoxEdriel <ga99_buggi@hotmail.com>
2026-03-15 20:05:08 -0500

    group para kafka logs

---
+++ Intercore.shared/Constans/KAFKA/groups/LogsKafkaConstant.cs
@@ -0,0 +1,6 @@
+namespace Intercore.shared.Constans.KAFKA.groups;
+
+public class LogsKafkaConstant
+{
+    public const string Id = "intercore-logs-service";
+}
--- Intercore.shared/obj/Debug/net9.0/Intercore.shared.AssemblyInfo.cs
+++ Intercore.shared/obj/Debug/net9.0/Intercore.shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intercore.shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f38c9f287d847e1cc8f8dc6679f84f871df76984")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e556800bee39a0728aebf795761f289a0c850ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intercore.shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intercore.shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
